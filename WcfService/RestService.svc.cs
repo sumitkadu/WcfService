de43ae52c71a802dda31078f3d5cdf50e7414136
Sumit Kadu (sumitkadu) <sumit.kadu@live.com>
2017-04-15 00:56:34 +0530

    Commit

---
+++ WcfService/RestService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.ServiceModel.Web;
+
+namespace WcfService
+{
+    public class RestService : IRestService
+    {   
+        public string GetData()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("Output.json"))
+                {
+                    string content = reader.ReadToEnd();
+                    reader.Close();                   
+                    return content;
+                }
+            }
+            catch
+            {
+                return "Something went wrong";
+            }
+        }
+    }
+}
